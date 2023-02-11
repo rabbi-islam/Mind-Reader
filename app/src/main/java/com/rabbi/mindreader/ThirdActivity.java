9db37ff9bb202fb4dc72aa9a96e748148b17331c
Rabbi <Rabbyislam7777@gmail.com>
2023-02-11 17:44:05 +0600

    validation for all edittext--> should be filled

--- app/src/main/java/com/rabbi/mindreader/ThirdActivity.java
+++ app/src/main/java/com/rabbi/mindreader/ThirdActivity.java
@@ -1,5 +1,7 @@
 package com.rabbi.mindreader;
 
+import static com.rabbi.mindreader.SecondActivity.InputValue;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -28,7 +30,7 @@ public class ThirdActivity extends AppCompatActivity {
         setContentView(R.layout.activity_third);
 
         intent = getIntent();
-        String msg = intent.getStringExtra(SecondActivity.InputValue);
+        String msg = intent.getStringExtra(InputValue);
         value= String.valueOf(msg);
         gettingNumberOfLetter= Integer.parseInt(msg);
 
@@ -48,8 +50,24 @@ public class ThirdActivity extends AppCompatActivity {
 
         //Hiding edittext here
         hideEditText();
+        settingValuesInRows();
 
 
+
+
+        submitBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                isValidte();
+            }
+        });
+
+
+
+
+
+    }
+    public void settingValuesInRows(){
         if (gettingNumberOfLetter == 3) {
             tableRow4.setVisibility(View.GONE);
             tableRow5.setVisibility(View.GONE);
@@ -69,18 +87,6 @@ public class ThirdActivity extends AppCompatActivity {
             settingAlphabetvalue4();
             settingAlphabetvalue5();
         }
-
-        submitBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-            }
-        });
-
-
-
-
-
     }
     public void hideEditText(){
         if (gettingNumberOfLetter==3){
@@ -225,5 +231,97 @@ public class ThirdActivity extends AppCompatActivity {
             TextView tv = findViewById(textViews5[i]);
             tv.setText(splitString3[i]);
         }
+    }
+
+    public void isValidte(){
+
+        num1 = editText1.getText().toString().trim();
+        num2 = editText2.getText().toString().trim();
+        num3 = editText3.getText().toString().trim();
+        num4 = editText4.getText().toString().trim();
+        num5 = editText5.getText().toString().trim();
+
+
+        if (gettingNumberOfLetter==3){
+            validationForThreeEditText();
+        }else if (gettingNumberOfLetter==4){
+            validationForFourEditText();
+        }else if (gettingNumberOfLetter==5){
+            validationForFiveEditText();
+        }
+
+    }
+
+
+    private void validationForThreeEditText(){
+        if ((num1.isEmpty()) || (num2.isEmpty()) || (num3.isEmpty())){
+
+            Toast.makeText(this, "field must not be empty!", Toast.LENGTH_SHORT).show();
+
+        } else if ((Integer.parseInt(num1)<1) || (Integer.parseInt(num1)>7)){
+
+            Toast.makeText(this, "num1 invalid!", Toast.LENGTH_SHORT).show();
+
+        }else if ((Integer.parseInt(num2)<1) || (Integer.parseInt(num2)>7)){
+
+            Toast.makeText(this, "num2 invalid!!", Toast.LENGTH_SHORT).show();
+        }else if ((Integer.parseInt(num3)<1) || (Integer.parseInt(num3)>7)){
+
+            Toast.makeText(this, "num3 invalid!!", Toast.LENGTH_SHORT).show();
+        }else{
+            sendValue();
+        }
+    }
+    private void validationForFourEditText(){
+        if ((num1.isEmpty()) || (num2.isEmpty()) || (num3.isEmpty()) || (num4.isEmpty()) ){
+
+            Toast.makeText(this, "field must not be empty!", Toast.LENGTH_SHORT).show();
+
+        } else if ((Integer.parseInt(num1)<1) || (Integer.parseInt(num1)>7)){
+
+            Toast.makeText(this, "num1 invalid!", Toast.LENGTH_SHORT).show();
+
+        }else if ((Integer.parseInt(num2)<1) || (Integer.parseInt(num2)>7)){
+
+            Toast.makeText(this, "num2 invalid!!", Toast.LENGTH_SHORT).show();
+        }else if ((Integer.parseInt(num3)<1) || (Integer.parseInt(num3)>7)){
+
+            Toast.makeText(this, "num3 invalid!!", Toast.LENGTH_SHORT).show();
+        }else if ((Integer.parseInt(num4)<1) || (Integer.parseInt(num4)>7)){
+
+            Toast.makeText(this, "num4 invalid!!", Toast.LENGTH_SHORT).show();
+        }else{
+
+            sendValue();
+        }
+    }
+    private void validationForFiveEditText(){
+        if ((num1.isEmpty()) || (num2.isEmpty()) || (num3.isEmpty()) || (num4.isEmpty()) || (num5.isEmpty()) ){
+
+            Toast.makeText(this, "field must not be empty!", Toast.LENGTH_SHORT).show();
+
+        } else if ((Integer.parseInt(num1)<1) || (Integer.parseInt(num1)>7)){
+
+            Toast.makeText(this, "num1 invalid!", Toast.LENGTH_SHORT).show();
+
+        }else if ((Integer.parseInt(num2)<1) || (Integer.parseInt(num2)>7)){
+
+            Toast.makeText(this, "num2 invalid!!", Toast.LENGTH_SHORT).show();
+        }else if ((Integer.parseInt(num3)<1) || (Integer.parseInt(num3)>7)){
+
+            Toast.makeText(this, "num3 invalid!!", Toast.LENGTH_SHORT).show();
+        }else if ((Integer.parseInt(num4)<1) || (Integer.parseInt(num4)>7)){
+
+            Toast.makeText(this, "num4 invalid!!", Toast.LENGTH_SHORT).show();
+        }else if ((Integer.parseInt(num5)<1) || (Integer.parseInt(num5)>7)){
+
+            Toast.makeText(this, "num5 invalid!!", Toast.LENGTH_SHORT).show();
+        }else{
+            sendValue();
+        }
+    }
+    public void sendValue(){
+        Toast.makeText(this, "Working!", Toast.LENGTH_SHORT).show();
+
     }
 }
