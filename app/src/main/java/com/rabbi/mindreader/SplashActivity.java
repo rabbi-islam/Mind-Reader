c98b5cb5176f1b311b934e7507f6d12e655494cf
Rabbi <Rabbyislam7777@gmail.com>
2023-02-12 19:29:11 +0600

    new icon in splash screen and new animation added

--- app/src/main/java/com/rabbi/mindreader/SplashActivity.java
+++ app/src/main/java/com/rabbi/mindreader/SplashActivity.java
@@ -5,24 +5,35 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 public class SplashActivity extends AppCompatActivity {
 
-    ImageView mindLogo;
+    TextView mindReaderText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
-        mindLogo = findViewById(R.id.mindLogo);
+        mindReaderText = findViewById(R.id.mindReaderText);
+        startAnimation();
 
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
+
                 startActivity(new Intent(SplashActivity.this,MainActivity.class));
+
             }
-        },3000);
+        },3500);
+    }
+
+    private void startAnimation(){
+        Animation animation = AnimationUtils.loadAnimation(this,R.anim.anim_for_splash_screen);
+        mindReaderText.startAnimation(animation);
     }
 }
